4617d0edeeeb3f10e5293623f4322ff0e21ce57d
Hirotaka Yoneda <squar37@gmail.com>
2017-09-13 21:28:41 +0900

    Added boosts

--- game/js/race-car.js
+++ game/js/race-car.js
@@ -6,12 +6,22 @@ var moves, cnt;
 var dir;
 var score, time_bonus;
 var speed_var;
-var coins;
-var boost;
+var get_coins;
+var boost, get_boosts;
 var mobx, mobr, mobc, mobf;
 var coinx, coinr, coinf;
+var boostx, boostr, boostf;
 var IMG_ARRAY = new Array();
-var IMG_SRC_ARRAY = new Array("highway.png", "car_mine.png", "car_red.png", "car_green.png", "car_blue.png", "goal.png");
+var IMG_SRC_ARRAY = new Array(
+	"highway.png",
+	"car_mine.png",
+	"car_red.png",
+	"car_green.png",
+	"car_blue.png",
+	"goal.png",
+	"coin.png",
+	"boost.png"
+);
 var IMG_COUNT = IMG_SRC_ARRAY.length;
 var IMG_CURRENT_COUNT = 0;
 var IMG_HIGHWAY;
@@ -20,11 +30,14 @@ var IMG_CAR_RED;
 var IMG_CAR_GREEN;
 var IMG_CAR_BLUE;
 var IMG_GOAL;
+var IMG_COIN;
+var IMG_BOOST;
 var MOVE_INTERVAL_VALUE;
 var GOAL = 10000; // meter
-var MOVE_PER_SEC = 30; // FPS
+var MOVE_PER_SEC = 50; // FPS
 var MOB_SPEED = 27 / 3.6; // meter per second
 var MAX_DEGREE = 40 / 180 * Math.PI;
+var BOOST_TIME = 5;
 var STATE = -1;
 for (var i = 0; i < IMG_COUNT; i++) {
 	IMG_ARRAY[i] = new Image();
@@ -51,7 +64,7 @@ document.onkeydown = function (event) {
 	}
 }
 function speed() {
-	return 500 / 3.6 * (1 - Math.pow(0.84, speed_var));
+	return 500 / 3.6 * (1 - Math.pow(0.84, speed_var)) * (boost > 1.0e-7 ? 1.2 : 1);
 }
 function init() {
 	STATE = -1;
@@ -80,6 +93,8 @@ function init_img() {
 	IMG_CAR_GREEN = IMG_ARRAY[3];
 	IMG_CAR_BLUE = IMG_ARRAY[4];
 	IMG_GOAL = IMG_ARRAY[5];
+	IMG_COIN = IMG_ARRAY[6];
+	IMG_BOOST = IMG_ARRAY[7];
 }
 function init_var() {
 	// --- SET INITIAL VARIABLES --- //
@@ -90,9 +105,10 @@ function init_var() {
 	row = row_fixed * 80;
 	dir = 0.0;
 	speed_var = 0;
-	coins = 0;
+	get_coins = 0;
+	get_boosts = 0;
 	score = time_bonus = 0;
-	boost = 1;
+	boost = 0;
 
 	// --- SET PLACE OF MOBS --- //
 	mobx = new Array();
@@ -131,6 +147,19 @@ function init_var() {
 		coinx.push(pos_coin);
 		coinf.push(true);
 	}
+
+	// --- SET PLACE OF BOOSTS --- //
+	boostx = new Array();
+	boostr = new Array();
+	boostf = new Array();
+	for (var i = 0; i < 5; i++) {
+		var lx = i * 1600 + 1000, rx = lx + 1200;
+		var px = Math.floor(Math.random() * (rx - lx) + lx);
+		var pr = Math.floor(Math.random() * 5);
+		boostx.push(px);
+		boostr.push(pr);
+		boostf.push(true);
+	}
 }
 function move() {
 	dist += speed() * Math.cos(dir) / MOVE_PER_SEC;
@@ -143,8 +172,9 @@ function move() {
 		mobx[i] += MOB_SPEED / MOVE_PER_SEC;
 	}
 	cnt++;
+	boost = Math.max(boost - 1 / MOVE_PER_SEC, 0);
 	if (dist < GOAL) {
-		speed_var += 1 / MOVE_PER_SEC;
+		speed_var += 1 / MOVE_PER_SEC * (boost > 1.0e-7 ? 3 : 1);
 		moves++;
 	}
 	else {
@@ -164,9 +194,15 @@ function move() {
 	var colc = collide_coin();
 	if (colc != -1) {
 		coinf[colc] = false;
-		coins++;
+		get_coins++;
 	}
-	score = Math.floor(dist * 4 + coins * 50);
+	var colb = collide_boost();
+	if (colb != -1) {
+		boostf[colb] = false;
+		boost = BOOST_TIME;
+		get_boosts++;
+	}
+	score = Math.floor(dist * 4 + get_coins * 50 + get_boosts * 500);
 	draw();
 }
 function collide_mob() {
@@ -182,6 +218,15 @@ function collide_coin() {
 		var dx = (dist - 20 <= cx && cx <= dist ? 0 : Math.min(Math.abs(cx - dist), Math.abs(cx - (dist - 20))));
 		var dy = (row - 25 <= cy && cy <= row + 25 ? 0 : Math.min(Math.abs(cy - (row - 25)), Math.abs(cy - (row + 25))));
 		if (coinf[i] && dx * dx + dy * dy / 25 <= 4.8 * 4.8) return i;
+	}
+	return -1;
+}
+function collide_boost() {
+	for (var i = 0; i < boostx.length; i++) {
+		var cx = boostx[i] - 24 / 5, cy = boostr[i] * 80;
+		var dx = (dist - 20 <= cx && cx <= dist ? 0 : Math.min(Math.abs(cx - dist), Math.abs(cx - (dist - 20))));
+		var dy = (row - 25 <= cy && cy <= row + 25 ? 0 : Math.min(Math.abs(cy - (row - 25)), Math.abs(cy - (row + 25))));
+		if (boostf[i] && dx * dx + dy * dy / 25 <= 4.8 * 4.8) return i;
 	}
 	return -1;
 }
@@ -221,18 +266,6 @@ function drawroad() {
 	context.drawImage(IMG_GOAL, 50, dist * 5 - GOAL * 5 + 350, 400, 30);
 	context.fillStyle = ""
 
-	// --- Draw Coins --- //
-	for (var i = 0; i < coinx.length; i++) {
-		var px = dist * 5 - coinx[i] * 5 + 380 + 30;
-		if (coinf[i] && -48 <= px && px <= 500) {
-			context.fillStyle = "#ffff66";
-			context.beginPath();
-			context.arc(coinr[i] * 80 + 90, px, 24, Math.PI * 0 / 180, Math.PI * 360 / 180, false);
-			context.fill();
-			context.closePath();
-		}
-	}
-
 	// --- Draw Mobs --- //
 	for (var i = 0; i < mobx.length; i++) {
 		var car_img;
@@ -247,32 +280,81 @@ function drawroad() {
 
 	// --- Draw Car --- //
 	context.drawImage(IMG_CAR_MINE, row + 65, 380, 50, 100);
+
+	// --- Draw Coins --- //
+	for (var i = 0; i < coinx.length; i++) {
+		var px = dist * 5 - coinx[i] * 5 + 380 + 24;
+		if (coinf[i] && -48 <= px && px <= 500) {
+			context.save();
+			context.beginPath();
+			context.arc(coinr[i] * 80 + 90, px, 24, Math.PI * 0 / 180, Math.PI * 360 / 180, false);
+			context.clip();
+			context.drawImage(IMG_COIN, coinr[i] * 80 + 66, px - 24, 48, 48);
+			context.restore();
+		}
+	}
+
+	// --- Draw Boosts --- //
+	for(var i = 0; i < boostx.length; i++) {
+		var px = dist * 5 - boostx[i] * 5 + 380 + 24;
+		if (boostf[i] && -48 <= px && px <= 500) {
+			context.save();
+			context.beginPath();
+			context.arc(boostr[i] * 80 + 90, px, 24, Math.PI * 0 / 180, Math.PI * 360 / 180, false);
+			context.clip();
+			context.drawImage(IMG_BOOST, boostr[i] * 80 + 66, px - 24, 48, 48);
+			context.restore();
+		}
+	}
 }
 function drawsidebar() {
+	// --- Basic Setup --- //
 	context.strokeStyle = "#ffdd99";
 	context.fillStyle = "#ffcccc";
 	context.fillRect(500, 0, 250, 500);
 	context.strokeRect(500, 0, 250, 140);
-	context.strokeRect(500, 140, 250, 240);
-	context.strokeRect(500, 380, 250, 120);
+	context.strokeRect(500, 140, 250, 150);
+	context.strokeRect(500, 290, 250, 100);
+	context.strokeRect(500, 390, 250, 110);
+
+	// --- Score --- //
 	context.font = "italic bold 50px fantasy";
 	context.fillStyle = "#333333";
 	context.textAlign = "left";
 	context.fillText("SCORE:", 510, 50);
 	context.textAlign = "right";
 	context.fillText(score, 730, 120);
+
+	// --- Informations --- //
+	context.fillStyle = "#333333";
 	context.textAlign = "left";
-	context.font = "normal normal 22px sans-serif"
+	context.font = "22px sans-serif"
 	context.fillText("Time(sec): " + Math.floor(moves / MOVE_PER_SEC * 100) / 100, 510, 175);
 	context.fillText("Distance(m): " + Math.min(Math.floor(dist), GOAL), 510, 215);
-	context.fillText("Coins: " + coins, 510, 255);
+	context.fillText("Coins: " + get_coins, 510, 255);
+
+	// --- Boost --- //
+	context.fillStyle = "#333333";
+	context.font = "20px sans-serif";
+	context.textAlign = "left";
+	context.fillText("Boost Time(sec): " + Math.floor(boost * 100) / 100, 510, 319);
+	var grd1 = context.createLinearGradient(520, 0, 730, 0);
+	grd1.addColorStop(0, "#44ffff");
+	grd1.addColorStop(1, "#4444ff");
+	context.fillStyle = grd1;
+	context.fillRect(520, 337, 210 * boost / BOOST_TIME, 33);
+	context.strokeStyle = "#555555";
+	context.strokeRect(520, 337, 210, 33);
+
+	// --- Speed --- //
+	context.fillStyle = "#333333";
 	context.font = "25px fantasy";
 	context.textAlign = "left";
 	context.fillText("Speed:  " + Math.round(speed() * 3.6) + "km/h", 510, 420);
-	var grd = context.createLinearGradient(520, 0, 730, 0);
-	grd.addColorStop(0, "orange");
-	grd.addColorStop(1, "red");
-	context.fillStyle = grd;
+	var grd2 = context.createLinearGradient(520, 0, 730, 0);
+	grd2.addColorStop(0, "orange");
+	grd2.addColorStop(1, "red");
+	context.fillStyle = grd2;
 	context.fillRect(520, 440, 210 * (speed() * 3.6 / 600), 40);
 	context.strokeStyle = "#555555";
 	context.strokeRect(520, 440, 210, 40);
